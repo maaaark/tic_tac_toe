7febe7a1182c8e129764503bb173cecc95069316
Mark <marktomicki@gmail.com>
2017-09-13 10:39:56 +0200

    bring in X and O's

--- source/app.jsx
+++ source/app.jsx
@@ -1,12 +1,39 @@
 import React, { Component } from "react";
 
 export default class App extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            P1_SYMBOL: "X",
+            P2_SYMBOL: "O",
+            currentPlayer: "X",
+            gameEnded: false,
+            totalMoves: 0,
+            board: [
+                "", "", "", "", "", "", "", "", ""
+            ]
+        }
+    }
+
+    playerMove(event) {
+        if(!this.state.gameEnded && this.state.totalMoves < 9) {
+            event.target.innerText= this.state.currentPlayer;
+            console.log(this.state.currentPlayer);
+            this.state.currentPlayer = this.state.currentPlayer === this.state.P1_SYMBOL ? this.state.P2_SYMBOL : this.state.P1_SYMBOL;
+            this.state.totalMoves++;
+            console.log(this.state.totalMoves);
+        } else {
+            console.log("game ended");
+        }
+    }
+
     render() {
         return (
             <div id="game">
               <h1>TIC TAC TOE</h1>
               <div id="board">
-                <div id="boardContainer">
+                <div id="boardContainer" onClick={(e)=>this.playerMove(e)}>
                   <div className="square"></div>
                   <div className="square"></div>
                   <div className="square"></div>
