7d3df66535b965bbeab6ec08476b279be61e1e05
Mark <marktomicki@gmail.com>
2017-09-13 13:38:05 +0200

    start and reset logic

--- source/app.jsx
+++ source/app.jsx
@@ -2,14 +2,19 @@ import React, { Component } from "react";
 
 export default class App extends Component {
 
+
+
     constructor() {
         super()
+
         this.state = {
+            gameStarted: false,
             P1_SYMBOL: "X",
             P2_SYMBOL: "O",
             currentPlayer: "X",
             gameEnded: false,
             totalMoves: 0,
+            winner: null,
             board: [
                 "", "", "", "", "", "", "", "", ""
             ]
@@ -17,23 +22,68 @@ export default class App extends Component {
     }
 
     playerMove(event) {
-        console.log(this.state.totalMoves);
-        if(!this.state.gameEnded && this.state.totalMoves <= 8) {
-
+        if(this.state.gameStarted){
             if(this.state.board[event.target.dataset.squareId] == "") {
                 this.state.board[event.target.dataset.squareId] = this.state.currentPlayer;
 
-                this.state.currentPlayer = this.state.currentPlayer === this.state.P1_SYMBOL ? this.state.P2_SYMBOL : this.state.P1_SYMBOL;
-                this.state.totalMoves++;
-                console.log(this.state.board);
-                this.setState({
-                    board: this.state.board,
-                })
-            }
+                if(this.checkWinner()) {
+                    this.setState({gameStarted: false})
+                    console.log("game stopped")
+                } else {
+                    this.state.currentPlayer = this.state.currentPlayer === this.state.P1_SYMBOL ? this.state.P2_SYMBOL : this.state.P1_SYMBOL;
+                    this.state.totalMoves++;
+                    // console.log(this.state.board);
+                    this.setState({
+                        board: this.state.board,
+                    })
+                }
 
-        } else {
-            console.log("game ended");
+            }
         }
+    }
+
+    startGame() {
+        console.log("start");
+        this.setState({
+            gameStarted: true,
+        })
+
+        var boardDiv = document.getElementById("board")
+        boardDiv.classList.add("activeGame")
+
+        var abortButton = document.getElementById("abort")
+        abortButton.classList.remove("hidden")
+
+        var startButton = document.getElementById("startButton")
+        startButton.classList.add("hidden")
+    }
+
+    checkWinner() {
+
+        var currentTurn = this.state.currentPlayer
+        var symbols = this.state.board
+        var winner = this.state.winner
+        var gameStarted = this.state.winner
+        var winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+
+        return winningCombos.find(function(combo) {
+            if(symbols[combo[0]] !== "" && symbols[combo[1]] !== ""  && symbols[combo[2]] !== ""  && symbols[combo[0]] === symbols[combo[1]] && symbols[combo[1]] === symbols[combo[2]]) {
+                console.log("winner found: "+currentTurn);
+                winner = currentTurn
+
+                return true
+            } else {
+                return false
+            }
+        })
+
+        /*
+        if(!this.state.gameEnded && this.state.totalMoves <= 8) {
+            console.log("no winner");
+        } else {
+            console.log("winner");
+        }
+        */
     }
 
     resetGame() {
@@ -43,14 +93,24 @@ export default class App extends Component {
                 "", "", "", "", "", "", "", "", ""
             ]
         })
+
+
+        var boardDiv = document.getElementById("board")
+        boardDiv.classList.remove("activeGame")
+
+        var abortButton = document.getElementById("abort")
+        abortButton.classList.add("hidden")
+
+        var startButton = document.getElementById("startButton")
+        startButton.classList.remove("hidden")
     }
 
     render() {
         return (
             <div id="game">
-              <h1>TIC TAC TOE</h1>
-              <div id="abort" onClick={(e)=>this.resetGame()}>RESET</div>
-              <div id="board">
+              <h1>TIC TAC</h1>
+              <div id="abort" className="hidden" onClick={(e)=>this.resetGame()}>RESET</div>
+              <div id="board" className="">
                 <div id="boardContainer" onClick={(e)=>this.playerMove(e)}>
                     {this.state.board.map((cell, index) => {
                         return <div data-square-id={index} className="square">{cell}</div>;
@@ -58,7 +118,7 @@ export default class App extends Component {
                 </div>
               </div>
               <div id="statusBar">
-                  <div id="startButton">START</div>
+                  <button id="startButton" onClick={(e)=>this.startGame(e)}>START</button>
               </div>
             </div>
         )
